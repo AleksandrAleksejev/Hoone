c06c69787a38b099e6611d11a01626201581db95
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2022-03-14 12:29:10 +0300

    g

--- Hoone/Program.cs
+++ Hoone/Program.cs
@@ -11,7 +11,8 @@ namespace Hoone
             EraMaja majake = new EraMaja();
             Inimene mees = new Inimene("Juku");
             majake.uks = new Uks("must");
-          //  p.korteriNum = new korteriNum(27);
+            //  p.korteriNum = new korteriNum(27);
+            //majaKorgus.uks = new Uks(20);
             mees.hoone = majake;
             mees.NaitaInfo();
         }
